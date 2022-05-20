80fb22b7ba13f19dcfce7f51b8fd35073b5cd427
Mohamed Adel Mostafa <mohamed.adel2137@gmail.com>
2022-05-21 00:41:23 +0200

    update

--- Structures/deque.cpp
+++ Structures/deque.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 using namespace std;
-// push_back
-// push_front
+// push_back (done)
+// push_front (done)
 // pop_front
 // pop_back
-// getback
-// getfront
+// getback (done)
+// getfront (done)
 // erase
 // clear
 // isempty (done)
 // find
+// display (done)
 class Node
 {
 public:
@@ -35,4 +36,67 @@ public:
     {
         return head == NULL;
     }
+    void pushFront(int item)
+    {
+        Node *newitem = new Node;
+        newitem->data = item;
+        if (isEmpty())
+        {
+            head = tail = newitem;
+        }
+        else
+        {
+            newitem->next = head;
+            head = newitem;
+        }
+    }
+
+    void pushBack(int item)
+    {
+        Node *newitem = new Node;
+        newitem->data = item;
+        if (isEmpty())
+        {
+            head = tail = newitem;
+        }
+        else
+        {
+            tail->next = newitem;
+            tail = newitem;
+        }
+    }
+    void disPlay()
+    {
+        if (isEmpty())
+            cout << "The list is Empty" << endl;
+        else
+        {
+            Node *temp = head;
+            while (temp != NULL)
+            {
+                cout << temp->data << " ";
+                temp = temp->next;
+            }
+            cout << endl;
+        }
+    }
+    int getFront()
+    {
+        return head->data;
+    }
+    int getBack()
+    {
+        return tail->data;
+    }
 };
+
+int main()
+{
+    int item;
+    Deque q;
+    q.pushFront(4);
+    q.pushFront(3);
+    q.pushBack(5);
+    q.pushBack(6);
+    q.disPlay();
+}
