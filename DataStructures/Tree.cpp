9ce40d963b905562d7d4c4c4451116220d09e8fb
modsyan <modclover7@gmail.com>
2022-05-22 12:35:59 +0200

    Stack

--- DataStructures/Tree.cpp
+++ DataStructures/Tree.cpp
@@ -4,8 +4,10 @@ using namespace std;
 template <class T>
 struct Node
 {
-    T data;
-    Node<T> *rigth = NULL, *left = NULL;
+    T value;
+    Node<T> *parent = NULL;
+    Node<T> *left = NULL;
+    Node<T> *rigth = NULL;
 };
 
 template <class T>
@@ -23,11 +25,41 @@ class Tree
         root->data = value;
     }
 
-    void insert(T data, Node<T>* tree) 
+    int insert(T value, Node<T>* node) 
     {
-        if (!tree)
+        Node<T> *newLeaf = new Node<T>;
+        newLeaf->data = value;
 
+        if(!node) {
+            node = newLeaf;
+            return 1;
+        }
+
+        if(data < node->data)
+        {
+            node->left = insert(node->left, value);
+            node->left->parent = node;
+            return 1;
+        }
+
+        node->rigth = insert(node->rigth, value);
+        node->rigth->parent = node;
+    }  
+
+    preOrder(Node<T>* node)
+    {
+        if(!node)
+            return -1;
+        cout << node->data << " ";
+        preOrder(node->left);
+        preOrder(node->rigth);
     }
-    
+
 
 };
+
+int main()
+{
+    Tree<int> *tree = new Tree<int>(2);
+    
+}
